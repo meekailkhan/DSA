a92d2928f54e636d29bb3054a790884e3d49c8fc
pc <meekailkhan123aar@gmail.com>
2025-01-30 23:44:55 +0530

    19th pattern solve

---
+++ patterns/19-pattern.js
@@ -0,0 +1,17 @@
+for(let i = 5 ; i >= 0 ; i--){
+    let space = " ".repeat(5-i);
+    let str = '';
+    for(let j = i ; j >= 0 ; j--){
+        str += '*'
+    }
+    console.log(str+space+space+str)
+}
+
+for(let i = 0 ; i <= 5 ; i++){
+    let space = " ".repeat(5-i);
+    let str = '';
+    for(let j = 0 ; j <= i ; j++){
+        str += '*'
+    }
+    console.log(str+space+space+str)
+}
